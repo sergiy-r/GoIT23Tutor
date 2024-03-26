053bb4c87f9cbe8c1e5ff216d080f915ab5b2e84
S <rusnakua@yahoo.com>
2024-03-26 19:16:22 +0000

    M4 HW

--- lesson_7/ex_3.py
+++ lesson_7/ex_3.py
@@ -6,7 +6,7 @@ from pathlib import Path
 folder = Path('./Temp')
 
 try:
-    with open(folder / 'temp.txt', 'r') as f:
+    with open(folder / 'temp.txt.txt', 'r') as f:
         for line in f:
             print(line, end='')
 except Exception as e:
@@ -14,5 +14,5 @@ except Exception as e:
 finally:
     print('Happy end')
 
-# /Users/vova/PycharmProjects/GoIT23/lesson7/Temp/temp.txt <- linux, macOS - 99,99%
-# D:\Documents\lesson7\Temp\temp.txt   <- Windows
+# /Users/vova/PycharmProjects/GoIT23/lesson7/Temp/temp.txt.txt <- linux, macOS - 99,99%
+# D:\Documents\lesson7\Temp\temp.txt.txt   <- Windows
